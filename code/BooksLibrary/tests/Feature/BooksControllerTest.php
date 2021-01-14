1e8e6d649c594a2d2430c9c04d22ad17d2675652
Luis Arenas <larenas97@gmail.com>
2021-01-14 20:56:36 +0000

    adding new test to see if it downloads book authors only

--- code/BooksLibrary/tests/Feature/BooksControllerTest.php
+++ code/BooksLibrary/tests/Feature/BooksControllerTest.php
@@ -150,4 +150,27 @@ class BooksControllerTest extends TestCase
         $response->assertOk();
     }
 
+    /** @test */
+    public function download_author_only_books_list_csv()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/books/add', [
+            'title' => 'A Different Book',
+            'author' => 'Jane Mary',
+        ]);
+        $response = $this->post('/books/add', [
+            'title' => 'An Untitled Book',
+            'author' => 'John Doe',
+        ]);
+
+        $response = $this->post('/books/add', [
+            'title' => 'Not the Same Book',
+            'author' => 'Jane K. Mary',
+        ]);
+        
+        $query = '/books/download/author';
+        $response = $this->get($query);
+        $response->assertOk();
+    }
+
 }
