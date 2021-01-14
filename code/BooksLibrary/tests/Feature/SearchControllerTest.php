08b7f011df5d94961cd8c6739858924cdc045d39
Luis Arenas <larenas97@gmail.com>
2021-01-14 13:00:42 +0000

    Added new test to search for books with a given author: - test queries for "John" - cleaned up unused code in BooksController

--- code/BooksLibrary/tests/Feature/SearchControllerTest.php
+++ code/BooksLibrary/tests/Feature/SearchControllerTest.php
@@ -41,5 +41,33 @@ class SearchControllerTest extends TestCase
        
     }
 
+     /** @test */
+     public function search_for_book_author()
+     {
+         $this->withoutExceptionHandling();
+         $response = $this->post('/books/add', [
+             'title' => 'A Different Book',
+             'author' => 'Jane Mary',
+         ]);
+         $response = $this->post('/books/add', [
+             'title' => 'An Untitled Book',
+             'author' => 'John Doe',
+         ]);
+ 
+         $response = $this->post('/books/add', [
+             'title' => 'Not the Same Book',
+             'author' => 'Jane K. Mary',
+         ]);
+         
+         $query = '/books/search?query=John';
+         $response = $this->get($query);
+     
+         $response->assertSeeText('An Untitled Book');
+         $response->assertDontSeeText('Not the Same Book', $escaped = true);
+         $response->assertDontSeeText('A Different Book', $escaped = true);
+         
+        
+     }
+
 
 }
